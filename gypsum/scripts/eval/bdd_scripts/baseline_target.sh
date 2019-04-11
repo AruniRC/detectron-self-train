6f706ebe9703857ab712481156c337555fc3fcdc
PCJohn <prithvichakra@gmail.com>
2019-04-11 10:11:48 -0400

    added baseline eval scripts

---
+++ gypsum/scripts/eval/bdd_scripts/baseline_target.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+#SBATCH --job-name=baseline_target_eval
+#SBATCH -o gypsum/logs/%j_baseline_target_eval.txt 
+#SBATCH -e gypsum/errs/%j_baseline_target_eval.txt
+#SBATCH -p 1080ti-short
+#SBATCH --gres=gpu:8
+#SBATCH --mem=100000
+##SBATCH --cpus-per-task=4
+##SBATCH --mem-per-cpu=4096
+
+python tools/test_net.py \
+    --set TEST.SCORE_THRESH 0.1 TRAIN.JOINT_TRAINING False TRAIN.GT_SCORES False \
+    --dataset bdd_peds_not_clear_any_daytime_val \
+    --cfg configs/baselines/bdd100k.yaml \
+    --load_ckpt <path_to_ckpt_file> \
+    --multi-gpu-testing \
+
